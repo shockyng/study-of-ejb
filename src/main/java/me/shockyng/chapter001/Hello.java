87f510b4a169bd53e26e7bf9ff28e9397120c817
Luan Santos <luanhenriquer8@gmail.com>
2022-12-11 23:25:40 +0000

    add the first bean

--- src/main/java/me/shockyng/chapter001/Hello.java
+++ src/main/java/me/shockyng/chapter001/Hello.java
@@ -3,7 +3,20 @@ package me.shockyng.chapter001;
 import javax.ejb.EJBObject;
 import java.rmi.RemoteException;
 
+/**
+ * This is the HelloBean remote interface
+ *
+ * This interface is what clients operate on when they interact with EJB object
+ * The container vendor will implement this interface; the implemented object
+ * is the EJB object, which delegates invacations to the actual bean.
+ */
+
 public interface Hello extends EJBObject {
 
-    String helloWorld() throws RemoteException;
+    /**
+     * The one method - hello - returns a greeting to the client.
+     * @return some greeting
+     * @throws RemoteException
+     */
+    String hello() throws RemoteException;
 }
